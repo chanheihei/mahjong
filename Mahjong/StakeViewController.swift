cff9f93835b599b0f4b04e2692346d9bc1c14089
KING <king.chan@asos.com>
2018-06-21 14:12:58 +0100

    remove collection view

--- Mahjong/StakeViewController.swift
+++ Mahjong/StakeViewController.swift
@@ -8,26 +8,9 @@
 
 import UIKit
 
-class StakeViewController: UIViewController, UICollectionViewDataSource {
-	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		if section == 0 {return 20}
-		return 0
-	}
-
-	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foobar", for: indexPath)
-		cell.backgroundColor = UIColor.cyan
-		return cell
-	}
-
-	@IBOutlet var pointsTable: UICollectionView!
-	var pointsTableValues = [1:2, 2:4, 3:8]
+class StakeViewController: UIViewController {
 	override func viewDidLoad() {
         super.viewDidLoad()
-		pointsTable.dataSource = self
-		pointsTable.register(PointsTableViewCell.self, forCellWithReuseIdentifier: "foobar")
-		pointsTable.reloadData()
-
         // Do any additional setup after loading the view.
     }
 
@@ -35,16 +18,4 @@ class StakeViewController: UIViewController, UICollectionViewDataSource {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
